6e4b576e67164748735f7f1d897d05aa677d4d9e
NBEriksson <72133203+NBEriksson@users.noreply.github.com>
2025-07-03 10:08:48 +0200

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int Year { get; set; }
+        public string Genre { get; set; } = null!;
+        public int Duration { get; set; }
+
+        //FK
+        public int MovieDetailsId { get; set; }
+
+        //Navigation prop
+        //public ICollection<Review> Reviews { get; set; } = new List<Review>();
+        //public ICollection<Actor> Actors { get; set; } = new List<Actor>();
+    }
+}
---
+++ Models/MovieDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApi.Models
+{
+    public class MovieDetails
+    {
+        public int Id { get; set; }
+        public string Synopsis { get; set; } = null!;
+        public string Language { get; set; } = null!;
+        public int Budget { get; set; }
+    }
+}
